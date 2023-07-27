ec207e7ef18788bdb18f5ae103ce55eb46ef97ae
Sowndharya <sowndharya.selladurai@syncfusion.com>
2023-07-27 12:21:50 +0530

    Added sample

---
+++ ImpactTestDemo/ImpactTestDemo/Model/ImpactTestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpactTestDemo
+{
+    public class ImpactTestModel
+    {
+
+        public double Machine { get; set; }
+
+        public List<double> EnergyAbsorptionValues { get; set; }
+
+        public ImpactTestModel(double machine, List<double> energyAbsorptionValues)
+        {
+            Machine = machine;
+            EnergyAbsorptionValues = energyAbsorptionValues;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Creating-a-Box-and-Whisker-Chart-for-Analyzing-the-Machine-Impact-Test
- Creating a Box and Whisker Chart for Analyzing the Machine Impact Test 
+The Machine Impact test is conducted to determine the fracture characteristics of raw materials under high strain rates. The purpose of this test is to assess the material's strength, resilience, and durability under real-world conditions where sudden impacts or shock loading may occur.
