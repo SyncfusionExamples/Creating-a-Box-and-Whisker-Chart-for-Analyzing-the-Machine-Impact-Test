52c3dc2299606b5c0896db4ed7a5a20eafadd5b6
Sowndharya <sowndharya.selladurai@syncfusion.com>
2023-10-05 12:56:34 +0530

    Update the nuget

--- ImpactTestDemo/ImpactTestDemo/App.xaml.cs
+++ ImpactTestDemo/ImpactTestDemo/App.xaml.cs
@@ -1,11 +1,12 @@
-﻿namespace ImpactTestDemo;
-
-public partial class App : Application
+﻿namespace ImpactTestDemo
 {
-	public App()
-	{
-		InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+            MainPage = new AppShell();
+        }
+    }
 }
